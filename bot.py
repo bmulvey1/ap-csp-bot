b707b65827ec9570a6c370586d985e18d4ae8397
Bea Mulvey <22732775+bmulvey1@users.noreply.github.com>
2017-12-08 20:54:27 -0500

    apparently can't python properly

--- bot.py
+++ bot.py
@@ -1,18 +1,20 @@
 import discord
 import re
 import conf
-import asycio
+import asyncio
 
-client = Discord.client()
+client = discord.Client()
 
 @client.event
-    async def on_ready():
-        print ('Logged in as')
-        print (client.user.name)
-        print (client.user.id)
-        print('-----')
+async def on_ready():
+    print ('Logged in as')
+    print (client.user.name)
+    print (client.user.id)
+    print('-----')
 
 @client.event
 async def on_message(message): #happens when a message is sent in any channel by any user
     print (message.content)
-    print ("sent by: " + message.author)    #placeholder
+    print ("sent by: " + str(message.author))    #placeholder
+
+client.run(conf.key)
