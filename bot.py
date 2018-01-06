4b66441ffc566d7eb7233c76190dfb070b8222e0
Bea Mulvey <22732775+bmulvey1@users.noreply.github.com>
2018-01-05 23:28:21 -0500

    Added checking for valid commands with responses, list of valid commands

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 import re
 import conf
 import asyncio
+import commands
 
 client = discord.Client()
 
@@ -15,16 +16,51 @@ async def on_ready():
 
 @client.event
 async def on_message(message): #happens when a message is sent in any channel by any user
+    commandPossible = False
+    i = 0
     print (message.content)
     print ("sent by: " + str(message.author) + " in channel " + str(message.channel))    #placeholder
     role_names = [role.name for role in message.author.roles] #create list of roles of author
-    if str(message.content[0]) == str(conf.symbol):     #check for valid commands
-        if ('Commander' in role_names):
-            print (str(message.author) + " is a commander, admin commands allowed") #can run more powerful commands
-            print (role_names)
-        else:
-            print (str(message.author) + " is not a commander, only user commands allowed") #can run more powerful commands
-            print (role_names)
-    else:
+    if message.author == client.user:
         pass
+    else:
+        if str(message.content[0]) == str(conf.symbol):     #check for valid commands
+            if ('Commander' in role_names):
+                print (str(message.author) + " is a commander, admin commands allowed") #can run more powerful commands
+                print (role_names) #For debugging
+                for command in commands.admin_commandlist:
+                    if message.content.find(command) != -1: #make sure the command is valid
+                        commandPossible = True
+                    else:
+                        if commandPossible == True:
+                            await client.send_message(message.channel, 'Recieved command from admin user')
+                            return
+                        elif i >= len(commands.admin_commandlist) and commandPossible != True:
+                            commandPossible = False
+                            msg = '{0.author.mention}, that isn\'t a valid command. Try running !help.'.format(message)
+                            await client.send_message(message.channel, msg)
+                        else:
+                            i=i+1
+                            print('Incremented i'+str(i))
+
+
+            else:
+                print (str(message.author) + " is not a commander, only user commands allowed") #can only run less powerful commands
+                print (role_names) #For debugging
+                for command in commands.user_commandlist:
+                    if message.content.find(command) != -1: #make sure the command is valid
+                        commandPossible = True
+                    else:
+                        if commandPossible == True:
+                            await client.send_message(message.channel, 'Recieved command from non-admin user')
+                            return
+                        elif i >= len(commands.user_commandlist) and commandPossible != True:
+                            commandPossible = False
+                            msg = '{0.author.mention}, that isn\'t a valid command. Try running !help.'.format(message)
+                            await client.send_message(message.channel, msg)
+                        else:
+                            i=i+1
+                            print('Incremented i'+str(i))
+        else:
+            pass
 client.run(conf.key)
---
+++ commands.py
@@ -0,0 +1,2 @@
+user_commandlist = ['help', 'test']
+admin_commandlist = ['help', 'remove', 'give_role', 'remove_role']
