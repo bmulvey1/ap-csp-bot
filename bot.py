05c5facf96fa544121f6f40b33c145ae35151314
Bea Mulvey <22732775+bmulvey1@users.noreply.github.com>
2018-01-09 23:00:36 -0500

    Run commands from external file

--- bot.py
+++ bot.py
@@ -17,6 +17,9 @@ async def on_ready():
 @client.event
 async def on_message(message): #happens when a message is sent in any channel by any user
     commandPossible = False
+    help_embed = discord.Embed(title='Help', type='rich', description='Help dialog', color=0x00ff00)
+    help_embed.add_field(name='Help', value='!help: Shows this help embed', inline=False)
+    help_embed.add_field(name='Test', value='!test: Does nothing', inline=False)
     i = 0
     print (message.content)
     print ("sent by: " + str(message.author) + " in channel " + str(message.channel))    #placeholder
@@ -26,7 +29,7 @@ async def on_message(message): #happens when a message is sent in any channel by
     else:
         if str(message.content[0]) == str(conf.symbol):     #check for valid commands
             if ('Commander' in role_names):
-                print (str(message.author) + " is a commander, admin commands allowed") #can run more powerful commands
+                print (str(message.author) + ' is a commander, admin commands allowed') #can run more powerful commands
                 print (role_names) #For debugging
                 for command in commands.admin_commandlist:
                     if message.content.find(command) != -1: #make sure the command is valid
@@ -34,15 +37,16 @@ async def on_message(message): #happens when a message is sent in any channel by
                     else:
                         if commandPossible == True:
                             await client.send_message(message.channel, 'Recieved command from admin user')
+                            if message.content.find('help') != -1:
+                              await commands.server_help(client, message, help_embed)
                             return
-                        elif i >= len(commands.admin_commandlist) and commandPossible != True:
-                            commandPossible = False
-                            msg = '{0.author.mention}, that isn\'t a valid command. Try running !help.'.format(message)
-                            await client.send_message(message.channel, msg)
                         else:
                             i=i+1
                             print('Incremented i'+str(i))
-
+                else:
+                    commandPossible = False
+                    msg = '{0.author.mention}, that isn\'t a valid command. Try running !help.'.format(message)
+                    await client.send_message(message.channel, msg)
 
             else:
                 print (str(message.author) + " is not a commander, only user commands allowed") #can only run less powerful commands
@@ -53,14 +57,18 @@ async def on_message(message): #happens when a message is sent in any channel by
                     else:
                         if commandPossible == True:
                             await client.send_message(message.channel, 'Recieved command from non-admin user')
+                            if message.content.find('help') != -1:
+                              await commands.server_help(client, message, help_embed)
                             return
-                        elif i >= len(commands.user_commandlist) and commandPossible != True:
-                            commandPossible = False
-                            msg = '{0.author.mention}, that isn\'t a valid command. Try running !help.'.format(message)
-                            await client.send_message(message.channel, msg)
                         else:
                             i=i+1
                             print('Incremented i'+str(i))
+                else:
+                    commandPossible = False
+                    msg = '{0.author.mention}, that isn\'t a valid command. Try running !help.'.format(message)
+                    await client.send_message(message.channel, msg)
         else:
             pass
+
+
 client.run(conf.key)
--- commands.py
+++ commands.py
@@ -1,2 +1,5 @@
 user_commandlist = ['help', 'test']
 admin_commandlist = ['help', 'remove', 'give_role', 'remove_role']
+
+async def server_help(client, message, help_embed):
+    await client.send_message(message.channel, embed=help_embed)
