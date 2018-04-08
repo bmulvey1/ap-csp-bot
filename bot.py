5480d448b4300b68b1e919ca6fadaac9554da3c5
Bea Mulvey <22732775+bmulvey1@users.noreply.github.com>
2018-04-08 15:37:38 -0400

    Add comments, start finalizing for submission to college board

--- bot.py
+++ bot.py
@@ -1,13 +1,19 @@
-import discord
-import re
-import conf
-import asyncio
-import commands
+try:
+    import discord # the discordpy library was created by https://github.com/Rapptz/; licenced under the MIT license
+except:
+    print('Please look at the README')
+import commands # to reduce visual complexity, commands are in this file
+try:
+    import conf # file for configuration; does not exist by default to avoid commiting the bot key to GitHub
+except: # runs if the config file doesn't exist
+    print('Please create the file `conf.py` and look at the README')
+import asyncio # Stock Python module, allows for single threaded concurrency
+import re # Stock Python module
 
 client = discord.Client()
 
 @client.event
-async def on_ready():
+async def on_ready(): # print info about the bot
     print ('Logged in as')
     print (client.user.name)
     print (client.user.id)
@@ -17,16 +23,13 @@ async def on_ready():
 @client.event
 async def on_message(message): #runs when a message is sent in any channel by any user
     commandPossible = False
-    help_embed = commands.create_embed('help')
-    rules_embed = commands.create_embed('rules')
-    admin_help = commands.create_embed('admin_help')
-    role_names = [role.name for role in message.author.roles] #create list of roles of author
+    role_names = [role.name for role in message.author.roles] #create list of roles of author of message
     if message.author == client.user: #ignore messages from bot
         pass
     else:
         print(message.content)
         print("sent by: " + str(message.author) + " in channel " + str(message.channel))
-        if str(message.content[0]) == str(conf.symbol):     #check for valid commands
+        if str(message.content[0]) == str(conf.symbol):     #check for valid commands, command symbol must be at the 0 index of the message
             if ('Commander' in role_names):
                 if conf.DEBUG: print (str(message.author)+ ' is a commander, admin commands allowed') #can run more powerful commands
                 if conf.DEBUG: print (role_names) #For debugging
@@ -34,13 +37,15 @@ async def on_message(message): #runs when a message is sent in any channel by an
                     if message.content.find(command) != -1: #make sure the command is valid
                         commandPossible = True
                         if conf.DEBUG: print(command)
-                    else:
+                    else: # validate command inputs, run commands
                         if commandPossible == True:
                             if message.content.find('help') != -1:
+                                admin_help = commands.create_embed('admin_help')
                                 await commands.server_embed(client, message, admin_help)
                                 return
 
                             elif message.content.find('rules') != -1:
+                                rules_embed = commands.create_embed('rules')
                                 await commands.server_embed(client, message, rules_embed)
                                 return
 
@@ -76,7 +81,7 @@ async def on_message(message): #runs when a message is sent in any channel by an
                     msg = '{0.author.mention}, that isn\'t a valid command. Try running !help.'.format(message) #tell message author that the command is invalid
                     await client.send_message(message.channel, msg)
 
-            else:
+            else: # different possible commands if user isn't an admin
                 print (str(message.author) + " is not a commander, only user commands allowed") #can only run less powerful commands
                 if conf.DEBUG: print (role_names) #For debugging
                 if conf.DEBUG: print (commands.user_commandlist)
@@ -86,8 +91,9 @@ async def on_message(message): #runs when a message is sent in any channel by an
                     else:
                         if commandPossible == True:
                             if message.content.find('help') != -1:
-                              await commands.server_embed(client, message, help_embed)
-                              return
+                                help_embed = commands.create_embed('help')
+                                await commands.server_embed(client, message, help_embed)
+                                return
 
                             elif message.content.find('rules') != -1:
                                 await commands.server_embed(client, message, rules_embed)
@@ -106,4 +112,4 @@ async def on_message(message): #runs when a message is sent in any channel by an
             pass
 
 
-client.run(conf.key)
+client.run(conf.key) # connect to Discord
--- commands.py
+++ commands.py
@@ -1,6 +1,6 @@
-import discord
-import conf
-import time
+import discord # the discordpy library was created by https://github.com/Rapptz/; licenced under the MIT license
+import conf # file for configuration
+import time # stock Python module
 
 user_commandlist = ['help', 'test', 'rules','poll', 'placeholder']
 admin_commandlist = ['help', 'remove', 'rules', 'give_role', 'take_role', 'poll', 'placeholder']
@@ -100,6 +100,7 @@ def create_embed(type):
         embed = discord.Embed(title='Help', type='rich', description='Help dialog', color=0x00ff00)
         embed.add_field(name='Help', value='!help: Shows this help embed', inline=False)
         embed.add_field(name='Test', value='!test: Does nothing', inline=False)
+        embed.add_field(name="Poll", value='!poll~ *text*~ *emoji*~ *emoji*: Creates a poll with 2 emoji as vote options', inline=False)
         return embed
     elif type == 'rules':
         embed = discord.Embed(title='rules', type='rich', description='Server rules', color=0x00ff00)
@@ -113,40 +114,52 @@ def create_embed(type):
         embed.add_field(name='Remove', value='!remove *messages* : Removes specified number of messages, up to 100', inline=False)
         embed.add_field(name='Give role', value='!give_role *user* *role* : Gives specified role to specified user', inline=False)
         embed.add_field(name='Remove role', value='!remove_role *user* *role* : Removes specified role from specified user', inline=False)
+        embed.add_field(name="Poll", value='!poll~ *text*~ *emoji*~ *emoji*: Creates a poll with 2 emoji as vote options', inline=False)
         return embed
 
 async def poll(client, message):
-    try:
-        text = message.content.split(' ', 4)[1]
-    except:
-        msg = "Please provide all fields necessary"
-        await client.send_message(message.channel, msg)
-        return
-    try:
-        emoji1 = message.content.split(' ', 4)[2]
-    except:
-        msg = "Please provide all fields necessary"
-        await client.send_message(message.channel, msg)
-        return
-    try:
-        emoji2 = message.content.split(' ', 4)[3]
-    except:
-        msg = "Please provide all fields necessary"
-        await client.send_message(message.channel, msg)
-        return
     server_channels = message.server.channels
-    server_emoji = message.server.emojis
     channels = {}
-    emojis = {}
     for channel in server_channels:
         channels[channel.name] = channel
+    try:
+        text = message.content.split('~ ', 4)[1]
+    except:
+        msg = "Please provide all fields necessary"
+        await client.send_message(message.channel, msg)
+        return
+    try:
+        bot_message1 = discord.utils.get(client.messages, content=text, author=client.user, channel=channels['polls'])
+    except:
+        msg = "{0.author.mention}, the `polls` channel may not exist, please contact a server admin or create the channel".format(message)
+        await client.send_message(message.channel, msg)
+        return
+    if bot_message1 != None:
+        print(bot_message1)
+        msg = "A poll with that text already exists, try looking at that poll or word yours differently"
+        await client.send_message(message.channel, msg)
+        return
+    try:
+        emoji1 = message.content.split('~ ', 4)[2]
+    except:
+        msg = "Please provide all fields necessary"
+        await client.send_message(message.channel, msg)
+        return
+    try:
+        emoji2 = message.content.split('~ ', 4)[3]
+    except:
+        msg = "Please provide all fields necessary"
+        await client.send_message(message.channel, msg)
+        return
+    server_emoji = message.server.emojis
+    emojis = {}
     for emoji in server_emoji:
         emojis[emoji.name] = emoji
     print(channels)
     print(emojis)
     await client.send_message(channels['polls'], text)
-    time.sleep(1)
-    bot_message = discord.utils.get(client.messages, content=text, author=client.user)
+    time.sleep(4)
+    bot_message = discord.utils.get(client.messages, content=text, author=client.user, channel=channels['polls'])
     await client.add_reaction(bot_message, emoji1)
     await client.add_reaction(bot_message, emoji2)
     return
